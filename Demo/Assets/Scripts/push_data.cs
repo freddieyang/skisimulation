36dcd554ca7f37df81f7d98057c083bbac9129fe
freddieyang <yf970113@outlook.com>
2019-11-27 21:43:06 +0800

    Upload the Unity project

---
+++ Demo/Assets/Scripts/push_data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push_data : MonoBehaviour
+{
+    private Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 euler_angles = tf.localRotation.eulerAngles;
+        Vector3 position = tf.localPosition;
+        string tex = conv(euler_angles.x);
+        string tey = conv(euler_angles.y);
+        string tez = conv(euler_angles.z);
+        string tpx = conv(position.x);
+        string tpy = conv(position.y);
+        string tpz = conv(position.z);
+        print(tpx + tpy + tpz + tez + tex + tey);
+    }
+
+    string conv(float x)
+    {
+        byte[] bytes = BitConverter.GetBytes(x);
+        Array.Reverse(bytes);
+        return (BitConverter.ToInt32(bytes, 0).ToString("X8"));
+    }
+}
