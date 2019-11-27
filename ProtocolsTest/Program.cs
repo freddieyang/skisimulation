d02897629ec6f847ee1955b37f2de771b3de5af0
freddieyang <yf970113@outlook.com>
2019-11-27 21:01:06 +0800

    Add the c# version of protocal

---
+++ ProtocolsTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProtocolsTest
+{
+    class Program
+    {
+
+        static string conv(float x, int c)
+        {
+            byte[] bytes = BitConverter.GetBytes(x);
+            Array.Reverse(bytes);
+            if (c == 0)
+                return (BitConverter.ToInt32(bytes, 0).ToString("X8"));
+            else
+                return (BitConverter.ToSingle(bytes, 0).ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            float floatValue = 30;
+            Console.WriteLine(conv(floatValue, 0));
+        }
+
+    }
+}
